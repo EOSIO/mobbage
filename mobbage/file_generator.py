81d13dce04f6976a2c3781b56285e9e09c55205a
Shawn Edge <shawn.edge@block.one>
2019-09-18 17:34:02 -0400

    updates

--- mobbage/file_generator.py
+++ mobbage/file_generator.py
@@ -1,6 +1,17 @@
 import os
+import random
+from time import perf_counter 
 
-
-def make_a_file(fname, fsize):
-  fh = open(fname, 'wb')
-  fh.write(os.urandom(fsize))
+t1_start = perf_counter()
+for x in range(0,1000):
+        fh = open(str(x), 'wb')
+        fh.write(os.urandom(36000))
+        fh.close()
+# Stop the stopwatch / counter 
+t1_stop = perf_counter() 
+  
+print("Elapsed time:", t1_stop, t1_start)  
+  
+  
+print("Elapsed time during the whole program in seconds:", 
+                                        t1_stop-t1_start)
